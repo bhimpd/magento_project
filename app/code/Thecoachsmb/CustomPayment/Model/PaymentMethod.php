fb327ecc463aa5e0373956e77c7dfe4a039a8604
bhimpd <dreamypd73@gmail.com>
2024-05-28 22:17:17 +0545

    custom payment integrated

---
+++ app/code/Thecoachsmb/CustomPayment/Model/PaymentMethod.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Thecoachsmb\CustomPayment\Model;
+
+/**
+ * Pay In Store payment method model
+ */
+class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
+{
+    /**
+     * Payment code
+     *
+     * @var string
+     */
+    protected $_code = 'testpayment';
+
+    /**
+     * Authorizes specified amount.
+     *
+     * @param InfoInterface $payment
+     * @param float         $amount
+     *
+     * @return $this
+     *
+     * @throws LocalizedException
+     */
+    public function authorize( \Magento\Payment\Model\InfoInterface $payment, $amount ) 
+    {
+        return $this;
+    }
+}
---
+++ app/code/Thecoachsmb/CustomPayment/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Thecoachsmb_CustomPayment',
+    __DIR__
+);
